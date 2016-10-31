85f4e60ce2167a520ed3c96d30283ea803452f38
Monobiao <xingji2163@163.com>
2016-10-31 15:55:42 +0800

    搭建框架，基于Spring boot.

---
+++ src/main/java/wl/hdzj/Hdzj2016Application.java
@@ -0,0 +1,14 @@
+package wl.hdzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Controller
+public class Hdzj2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hdzj2016Application.class, args);
+	}
+}
